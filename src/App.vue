c6b66ab8c269a1f996620cbd42abcd70236cb418
CXLDILEI <451202165@qq.com>
2019-07-13 16:42:36 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/Index.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
@@ -23,6 +22,28 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+@media (min-width: 768px){ 
+  .banner-content{
+    padding: 0 300px;
+  }
+  
+}
+
+/* 中等屏幕（桌面显示器，大于等于 992px） */
+@media (min-width: 992px) {
+
+}
+
+/* 大屏幕（大桌面显示器，大于等于 1200px） */
+@media (min-width:1200px) { 
+  
+ }
+ @media (max-width: 992px)and(min-width:375px) {
+   
+}
+ /* 小于375 iphone-6,7,8 */
+ @media (max-width: 375px) {
+
+ }
 </style>
---
+++ src/components/Index.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
+      <b-navbar-brand href="#">
+        <img class="logo" src="../assets/logo-ci@2x.png" alt="">
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Index</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+  </b-navbar>
+  <!-- 轮播区 -->
+  <div class="banner">
+    <div class="banner-content">
+      <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="3000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img
+          slot="img"
+          class="d-block img-fluid w-100"
+          width="1024"
+          height="480"
+          src="https://picsum.photos/1024/480/?image=55"
+          alt="image slot"
+        >
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+    </div>
+    
+  </div>
+  <!-- 底部联系 -->
+   <div class="bottom">
+      <div class="bottom-logo">
+        <img class="" src="../assets/logo-ci@2x.png" alt="">
+      </div>
+      <div class="bottom-info">
+        <div class="bottom-info-connection">联系方式</div>
+        <ul>
+          <li>CATEGORY</li>
+          <li>製品一覧</li>
+          <li>フィギュア</li>
+          <li>アルタイル</li>
+        </ul>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo{
+  width: 100px; 
+}
+.banner{
+  width: 100%;
+  background:-webkit-linear-gradient(#fff,#e7e7e7);
+}
+.banner-content{
+  margin-top: 56px;
+}
+.bottom{
+  display: flex;
+  width: 100%;
+  padding: 40px;
+  background-color: #222;
+  color: #fffff8;
+}
+
+.bottom-logo img{
+  width: 200px;
+}
+.bottom-info{
+  flex: 1;
+  width: 300px;
+  padding: 0 15px;
+}
+.bottom-info ul{
+  list-style: none;
+  padding: 0;
+}
+.bottom-info ul li{
+  font-size: 12px;
+}
+</style>
